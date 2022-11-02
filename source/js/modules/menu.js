db7d5a85838d18d105d96e0b2b8cee623c81a4f7
Ekaterina <ektregubova@gmail.com>
2022-11-02 23:29:39 +0500

    add js menu

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,16 +1,20 @@
 let header = document.querySelector('[data-header]');
 let toggleNav = header.querySelector('[data-nav-toggle]');
+let intro = document.querySelector('[data-intro]');
 
 header.classList.remove('no-js');
+header.classList.add('closed');
 
 function clickOnToggle() {
   toggleNav.addEventListener('click', function () {
     if (header.classList.contains('closed')) {
       header.classList.remove('closed');
       header.classList.add('opened');
+      intro.classList.add('closed');
     } else {
       header.classList.add('closed');
       header.classList.remove('opened');
+      intro.classList.remove('closed');
     }
   });
 }
