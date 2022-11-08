846558a71a49652b72e3e75522797581c406bc0a
Ekaterina <ektregubova@gmail.com>
2022-11-08 12:35:36 +0500

    img webp

--- gulp/optimizeImages.mjs
+++ gulp/optimizeImages.mjs
@@ -39,7 +39,7 @@ const sprite = () =>
 */
 
 const createWebp = () => {
-  const root = 'content/';
+  const root = '';
   return gulp
       .src(`source/img/${root}**/*.{png,jpg}`)
       .pipe(webp({quality: 90}))
