2376b2b07fbdf61d5befb05e719d5fbbd425252c
Ekaterina <ektregubova@gmail.com>
2022-11-13 19:48:04 +0500

    focus

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -1,7 +1,10 @@
+import {FocusLock} from '../utils/focus-lock';
 const header = document.querySelector('[data-header]');
 const toggleNav = document.querySelector('[data-nav-toggle]');
 const intro = document.querySelector('[data-intro]');
 const headerLinks = document.querySelectorAll('[data-header-link]');
+
+const focus = new FocusLock();
 
 if (header) {
   header.classList.remove('no-js');
@@ -12,12 +15,14 @@ function openMenu() {
   header.classList.remove('is-closed');
   header.classList.add('is-opened');
   intro.classList.add('is-closed');
+  focus.lock('[data-header]');
 }
 
 function closeMenu() {
   header.classList.add('is-closed');
   header.classList.remove('is-opened');
   intro.classList.remove('is-closed');
+  focus.unlock();
 }
 
 function clickOnToggle() {
